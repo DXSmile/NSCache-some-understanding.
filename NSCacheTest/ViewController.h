dee22af228bdf9c4f06ec08b49195b4bcdd44a81
DXSmile <312692347@qq.com>
2016-02-17 13:13:47 +0800

    NSCache
    NSCache

---
+++ NSCacheTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSCacheTest
+//
+//  Created by xiongdexi on 16/2/17.
+//  Copyright © 2016年 DXSmile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
